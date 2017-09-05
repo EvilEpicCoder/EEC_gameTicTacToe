3d2615bc2f95006c18109431c307ae2d023dd662
EvilEpicCoder <1st.elixile@gmail.com>
2017-09-06 01:07:56 +0300

    js AI part

--- ttt.js
+++ ttt.js
@@ -2,6 +2,7 @@ var playerSign;
 var queueActions=0;
 var globalIndexMatrix=[];
 var globalHitMatrix=[];
+var winArray=[[0,1,2],[3,4,5],[0,4,8],[2,4,6],[6,7,8],[0,3,6],[1,4,7],[2,5,8]].sort();
 $(document).ready(function(){
 	//console.log($('div.row'));
 	//console.log($("div.row:eq("+0+"):nth-child(1)").html());
@@ -14,8 +15,9 @@ $(document).ready(function(){
 	}
 	****/
 	$("span").click(function(){
-		checkWinner(globalIndexMatrix,globalHitMatrix);
-		console.log(this.classList);
+		//console.log(winArray);
+		
+		//console.log(this.classList);
 		if(queueActions%2==0){
 			if(this.classList==""){
 			$(this).addClass("circle");
@@ -28,6 +30,8 @@ $(document).ready(function(){
 			$("li:eq('0')").removeClass("gray");
 			$("li:eq('1')").removeClass("red");
 			queueActions++;	
+			
+			checkWinner(globalIndexMatrix,globalHitMatrix);
 			}else{
 			
 			}
@@ -41,7 +45,9 @@ $(document).ready(function(){
 			$("li:eq('0')").addClass("gray");
 			$("li:eq('1')").removeClass("gray");
 			$("li:eq('0')").removeClass("red");
-			queueActions++;	
+			queueActions++;
+			
+			checkWinner(globalIndexMatrix,globalHitMatrix);
 			}else{
 			
 			}
@@ -63,21 +69,33 @@ $(document).ready(function(){
 	* ***/
 	//console.log($("span:eq("+0+")"));
 });
-var ddd=0;
+
 function checkWinner(arr1,arr2){
 	
-	console.log(arr1);
-	console.log(arr2);
-	var dd=0;
+	//console.log(arr1);
+	//console.log(arr2);
+	var tArr=[];
+	var ddd=0;
 	for(var a=0;a<=arr2.length-1;a++){
-		
+		var dd=0;
 		if(arr2[a]>=1){
 			//console.log(parseInt(arr1[a]));
-			dd+=parseInt(arr1[a]);
 			
+			dd+=parseInt(arr1[a]);
+			tArr.push(dd);
+			console.log(tArr);
 		}
 		ddd+=dd;
-		console.log(ddd+"HERE");
+		for(var x=0;x<=winArray.length-1;x++){
+			//console.log(winArray[x].toString());
+			//console.log(tArr.toString()+"------------");
+			if(tArr.toString()==winArray[x].toString()){
+			
+			//console.log(tArr+"------------");
+			console.log("Winner is Mister TODD");
+			}
+		}
+		
 	}
 	//return mNum;
 	
