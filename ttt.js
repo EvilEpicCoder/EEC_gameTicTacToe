792ea64b37695ab019d50b9585f032ec24ba71f6
EvilEpicCoder <1st.elixile@gmail.com>
2017-09-10 07:55:18 +0300

    Added game vs computer and player

--- ttt.js
+++ ttt.js
@@ -1,113 +1,174 @@
-var playerSign;
+var playerSignSide;
 var queueActions=0;
-var globalIndexMatrix=[];
-var globalHitMatrix=[];
+var globalPlayer1=[];
+var globalPlayer2=[];
 var winArray=[[0,1,2],[3,4,5],[0,4,8],[2,4,6],[6,7,8],[0,3,6],[1,4,7],[2,5,8]].sort();
+var newArr=[];
+var tArr=[];
+var globalCounter=0;
+var boolComputer=false;
+var boolPlayerFirst=true;
+var boolPlayerZero=true;
+var boolGameRunning=true;
+var gameplayMode=0;
+var magikX;
+var totalTurns=0;
+
 $(document).ready(function(){
-	//console.log($('div.row'));
-	//console.log($("div.row:eq("+0+"):nth-child(1)").html());
-	/***
-	for(var i=0;i<=$("span").length-1;i++){
-		$("span:eq("+i+")").click(function(){
-		$(this).addClass("cross");
-		console.log(this.classList);
-		});
-	}
-	****/
-	$("span").click(function(){
-		//console.log(winArray);
+	magikX=Math.floor(Math.random() * (8 - 0)) + 0;
+	$("#Gameplay").val(0);
+	$("#Gameplay").change(function(){
+		gameplayMode=$("#Gameplay").val();
+		//console.log(gameplayMode+"-----------------------------------------------");
+	});
+	$(".btn").click(function(){
+		clearAll(true);
+	});
+	//if(boolGameRunning==false){
 		
-		//console.log(this.classList);
+	//}else{
+		//-----------------------------game Running-------------
+	$("span").click(function(){
+		
 		if(queueActions%2==0){
-			if(this.classList==""){
+			if(this.classList==""&&boolGameRunning==true){
 			$(this).addClass("circle");
-			//checkWinner("span","circle");
-			globalIndexMatrix.push($("span").index(this));
-			globalHitMatrix.push(1);
-			
+			totalTurns++;
+			globalPlayer1.push($("span").index(this));
 			$("li:eq('0')").addClass("red");
 			$("li:eq('1')").addClass("gray");
 			$("li:eq('0')").removeClass("gray");
 			$("li:eq('1')").removeClass("red");
-			queueActions++;	
-			
-			checkWinner(globalIndexMatrix,globalHitMatrix);
-			}else{
-			
-			}
-		}else{
-			if(this.classList==""){
-			$(this).addClass("cross");
-			globalIndexMatrix.push($("span").index(this));
-			globalHitMatrix.push(0);
-			
-			$("li:eq('1')").addClass("red");
-			$("li:eq('0')").addClass("gray");
-			$("li:eq('1')").removeClass("gray");
-			$("li:eq('0')").removeClass("red");
 			queueActions++;
-			
-			checkWinner(globalIndexMatrix,globalHitMatrix);
-			}else{
+			checkWinner(globalPlayer1,"Zero");
+			if(gameplayMode>0&&boolGameRunning==true){
+			smartComputer();
 			
 			}
 		}
+		}else{
+			if(gameplayMode==0){
+				if(this.classList==""&&boolGameRunning==true){
+				$(this).addClass("cross");
+				totalTurns++;
+				globalPlayer2.push($("span").index(this));
+			
+				$("li:eq('1')").addClass("red");
+				$("li:eq('0')").addClass("gray");
+				$("li:eq('1')").removeClass("gray");
+				$("li:eq('0')").removeClass("red");
+				queueActions++;
+				checkWinner(globalPlayer2,"Cross");
+				
+							
+			}
+		}
 		
+		}
 		
+		});
 		
-		
-	//globalHitMatrix.findIndex(isBigEnough);
-		//console.log();
-		//console.log(this.classList);
-	});
-	/**$("span").click(function(event){
-		//x.target.addClass("cross");
-		var target = event.target;
-		target.addClass("circle");
-		
-	});
-	* ***/
-	//console.log($("span:eq("+0+")"));
+		//}//-------------------------game Running End section
 });
 
-function checkWinner(arr1,arr2){
-	
-	//console.log(arr1);
-	//console.log(arr2);
-	var tArr=[];
-	var ddd=0;
-	for(var a=0;a<=arr2.length-1;a++){
-		var dd=0;
-		if(arr2[a]>=1){
-			//console.log(parseInt(arr1[a]));
+function checkWinner(player,playerSign){
+tArr=player;
+ if(tArr.length==3){
+	 compareArrayQuick(tArr,winArray,playerSign);
+  }else if(tArr.length>=4){
+    //console.log("Hardcore Function");
+      for(var i=0;i<=winArray.length-1;i++){
+		 for(var k=0;k<=winArray[i].length-1;k++){
+			 //console.log(winArray[i][k]+"winArray"+"["+i+"]"+"["+k+"]"+"-----"+tArr);
+			  if(tArr.includes(winArray[i][k])==true){
+			  globalCounter++;
+				//console.log(globalCounter+"globalCounter------------------------");
+				}
+		 }
+		 if(globalCounter>=3){
+			 //console.log("Hardcore Winner"+playerSign);
+			if(playerSign=="Zero"){
+				$("li:eq(0)").append(" Winner");
+				boolGameRunning=false;
+				alert("Winner Zero");
+				xflag=100;
+			}else{
+				$("li:eq(1)").append(" Winner");
+				boolGameRunning=false;
+				alert("Winner Cross");
+				xflag=100;
+			}
 			
-			dd+=parseInt(arr1[a]);
-			tArr.push(dd);
-			console.log(tArr);
-		}
-		ddd+=dd;
-		for(var x=0;x<=winArray.length-1;x++){
-			//console.log(winArray[x].toString());
-			//console.log(tArr.toString()+"------------");
-			if(tArr.toString()==winArray[x].toString()){
+		 }
+		globalCounter=0;
+		  
+		 //for ---
+	  }
+	  
+  }
+  
+}
+
+function compareArrayQuick(userArr,dataArr,playerSign){
+	 //console.log(userArr+"userArr");
+	 //console.log(dataArr+"dataArr");
+	   for(var shortWay=0;shortWay<=winArray.length-1;shortWay++){
+		   //console.log(shortWay+"ooooooooooooo");
+		  
+		   var compA=userArr.toString();
+		   var compB=dataArr[shortWay].toString();
+
+			if(compA==compB){
+			console.log("Winner Short"+playerSign);
+			if(playerSign=="Zero"){
+				$("li:eq(0)").append(" Winner");
+				boolGameRunning=false;
+				alert("Winner Zero");
+				
+			}else{
+				$("li:eq(1)").append(" Winner");
+				boolGameRunning=false;
+				alert("Winner Cross");
+			}
 			
-			//console.log(tArr+"------------");
-			console.log("Winner is Mister TODD");
 			}
 		}
-		
+}
+function clearAll(param){
+	if(param==true){
+		var playerSignSide;
+queueActions=0;
+globalPlayer1=[];
+globalPlayer2=[];
+winArray=[[0,1,2],[3,4,5],[0,4,8],[2,4,6],[6,7,8],[0,3,6],[1,4,7],[2,5,8]].sort();
+newArr=[];
+tArr=[];
+globalCounter=0;
+boolComputer=false;
+boolPlayerFirst=true;
+boolPlayerZero=true;
+boolGameRunning=true;
+gameplayMode=0;
+magikX=0;
+		boolGameRunning=true;
+		$("span").removeClass("circle cross");
+		$("li:eq(0)").text("Player 1 (Zero)");
+		$("li:eq(1)").text("Player 2 (Coss)");
+		$("#Gameplay").val(0);
 	}
-	//return mNum;
-	
-	/***console.log($(elementName).classList);
-	console.log(className);
-	var x=[];
-	for(var total=0;total<=$(elementName).length-1;total++){
-		if($(elementName).classList==className){
-			x.push("1");
-		}else{
-			x.push("0");
-		}
+boolGameRunning==true;
+}
+
+function smartComputer(){
+	magikX=Math.floor(Math.random() * (8 - 0)) + 0;
+	if($("span:eq("+magikX+")").hasClass("circle")==true||$("span:eq("+magikX+")").hasClass("cross")==true){
+	smartComputer();
+	}else{
+	//console.log(magikX+"computer thinking");
+	globalPlayer2.push(magikX);
+	$("span:eq("+magikX+")").addClass("cross");
+	queueActions++;
+	checkWinner(globalPlayer2,"Cross");
+	totalTurns++;
 	}
-	console.log(x);*///
 }
